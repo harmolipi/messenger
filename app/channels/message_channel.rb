b58b39aa49fe7c6afe032fb0a3c505501cf18885
harmolipi <nikob381@gmail.com>
2021-10-15 11:16:23 -0400

    Send and display messages via ActionCable

--- app/channels/message_channel.rb
+++ app/channels/message_channel.rb
@@ -1,9 +1,16 @@
 class MessageChannel < ApplicationCable::Channel
+  # before_filter :authenticate_user!
+
   def subscribed
     stream_from 'message'
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  def receive(data)
+    data['user'] = current_user
+    ActionCable.server.broadcast('message', data)
   end
 end
