556ed4b841f07fdaa0f4573431e729ddcc1244de
harmolipi <nikob381@gmail.com>
2021-10-15 11:16:05 -0400

    Fixed wrong method name

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,7 +2,7 @@ module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
 
-    def Connection
+    def connect
       self.current_user = find_verified_user
     end
 
