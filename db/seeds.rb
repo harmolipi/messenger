31a5c408b5fbc69d5c33ac18feaf1d497b8ae4c8
harmolipi <nikob381@gmail.com>
2021-10-14 22:54:29 -0400

    Set up devise gem and seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'mrcoolguy@aol.com', password: 'coolestontheplanet')
+User.create(email: 'mrmediocreguy@gmail.com', password: 'nottooshabby')
